18026cb799471c1011f89fec7fdf4ceecf3b0c49
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-04-13 17:46:58 +0800

    updated awsyam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <h3 className="text-2xl font-bold">Updated Version→</h3>
             <div className="text-lg">
               Just the basics - Everything you need to know to set up your
               database and authentication.
